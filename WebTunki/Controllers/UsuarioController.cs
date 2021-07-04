d7b54f6f80c7ae184105386325aa99a663e3c877
AbelardoCondoriFernandez <abelardocf0110@gmail.com>
2021-07-03 19:16:36 -0500

    update UsuarioController

--- WebTunki/Controllers/UsuarioController.cs
+++ WebTunki/Controllers/UsuarioController.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebTunki.Models;
+using WebTunki.transfer;
 
 namespace WebTunki.Controllers
 {
-    public class UsuarioController
+    public class UsuarioController : ApiController
     {
+        [HttpPost]
+        [Route("api/tunki/registrarusuario")]
+        public usuariodto RegistrarUsuario(usuariodto usuariodto )
+        {
+            return usuariosoa.RegistroUsuario(usuariodto);
+        }
+        [HttpGet]
+        [Route("api/tunki/listarusuario")]
+        public IEnumerable<usuariodto> ListarUsuario()
+        {
+            return usuariosoa.ListarUsuarios();
+        }
     }
 }
